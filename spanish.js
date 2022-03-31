e713916a7fb0b407950b6200f9d1e66ed8858713
Thon Ly <thonly@heartbank.ceo>
2022-03-30 21:59:58 -0700

    version 2

--- spanish.js
+++ spanish.js
@@ -1,3 +1,9 @@
 function updateLanguage(event) {
     document.location = event.target.value;
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F7HF2LJMCB');
